3b32297e37915205bdc96991e598eca121a162e2
Bruno Vieira <brunovieiracrz@gmail.com>
2022-05-22 22:32:47 -0300

    + creates initial configuration for viewpager

--- home/src/main/java/com/husqvarna/home/ui/activities/HomeActivity.kt
+++ home/src/main/java/com/husqvarna/home/ui/activities/HomeActivity.kt
@@ -1,12 +1,18 @@
-package com.husqvarna.home
+package com.husqvarna.home.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.husqvarna.home.databinding.ActivityHomeBinding
+import com.husqvarna.home.ui.views.MarginScalePageTransformer
+import com.husqvarna.movie.ui.adapters.MovieAdapter
 
 class HomeActivity : AppCompatActivity() {
     private var activityHomeBinding: ActivityHomeBinding? = null
     private val binding by lazy { activityHomeBinding!! }
+
+    private val viewpagerMargin by lazy {
+        resources.getDimension(com.husqvarna.tokens.R.dimen.spacing_eight).toInt()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +21,13 @@ class HomeActivity : AppCompatActivity() {
                 .inflate(layoutInflater)
                 .also { activityHomeBinding = it }
                 .root)
+
+        setupViewPager()
+    }
+
+    private fun setupViewPager() = binding.run {
+        homeViewPager.adapter = MovieAdapter(this@HomeActivity)
+        homeViewPager.offscreenPageLimit = 2
+        homeViewPager.setPageTransformer(MarginScalePageTransformer(viewpagerMargin))
     }
 }
