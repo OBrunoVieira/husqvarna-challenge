9262a439268210b0e0ed2623dfa070a2240d3fe9
Bruno Vieira <brunovieiracrz@gmail.com>
2022-05-23 23:57:03 -0300

    + migrate common configuration to core's module

--- core/src/main/java/com/husqvarna/core/network/NetworkConfiguration.kt
+++ core/src/main/java/com/husqvarna/core/network/NetworkConfiguration.kt
@@ -1,10 +1,24 @@
 package com.husqvarna.core.network
 
 import android.content.Context
+import com.husqvarna.core.BuildConfig.DEBUG
+import com.husqvarna.core.BuildConfig.HOST_TMDB
 import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+
+inline fun <reified T> createApi(context: Context) = run {
+    val retrofit = Retrofit.Builder()
+        .baseUrl(HOST_TMDB)
+        .client(buildOkHttpClient(DEBUG, context))
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    retrofit.create(T::class.java)
+}
 
 fun buildOkHttpClient(isDebug: Boolean, context: Context) = run {
     val loggingInterceptor =
--- movie/src/main/java/com/husqvarna/movie/di/NetworkModule.kt
+++ movie/src/main/java/com/husqvarna/movie/di/NetworkModule.kt
@@ -1,25 +1,10 @@
 package com.husqvarna.movie.di
 
-import android.content.Context
-import com.husqvarna.core.network.buildOkHttpClient
-import com.husqvarna.movie.BuildConfig.DEBUG
-import com.husqvarna.movie.BuildConfig.HOST_TMDB
+import com.husqvarna.core.network.createApi
 import com.husqvarna.movie.network.MoviesDataSource
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 val moviesNetworkModule = module {
-    factory { createMoviesApi(androidContext()) }
-}
-
-private fun createMoviesApi(context: Context) = run {
-    val retrofit = Retrofit.Builder()
-        .baseUrl(HOST_TMDB)
-        .client(buildOkHttpClient(DEBUG, context))
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    retrofit.create(MoviesDataSource::class.java)
+    factory { createApi<MoviesDataSource>(androidContext()) }
 }
