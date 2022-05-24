0053afe62b04441ff21b16c3eeec84600f702768
Bruno Vieira <brunovieiracrz@gmail.com>
2022-05-24 12:01:48 -0300

    + updates android manifest

--- app/src/main/java/com/husqvarna/challenge/MovieApplication.kt
+++ app/src/main/java/com/husqvarna/challenge/MovieApplication.kt
@@ -1,10 +1,11 @@
 package com.husqvarna.challenge
 
 import android.app.Application
-import com.husqvarna.movie.di.moviesInteractorModule
-import com.husqvarna.movie.di.moviesNetworkModule
-import com.husqvarna.movie.di.moviesRepository
-import com.husqvarna.movie.di.moviesViewModel
+import com.husqvarna.movie.di.*
+import com.husqvarna.movie_details.di.movieDetailInteractorModule
+import com.husqvarna.movie_details.di.movieDetailNetworkModule
+import com.husqvarna.movie_details.di.movieDetailRepository
+import com.husqvarna.movie_details.di.movieDetailViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
@@ -18,7 +19,10 @@ class MovieApplication : Application() {
             androidLogger()
             modules(
                 //Movies Module
-                moviesNetworkModule, moviesRepository, moviesInteractorModule, moviesViewModel
+                moviesNetworkModule, moviesRepository, moviesInteractorModule, moviesViewModel,
+                //Movie's detail
+                movieDetailNetworkModule, movieDetailRepository, movieDetailInteractorModule,
+                movieDetailViewModel
             )
         }
     }
