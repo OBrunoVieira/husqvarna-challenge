6be7b2908528e9e41567518a0dbf301055c787c4
Bruno Vieira <brunovieiracrz@gmail.com>
2022-05-24 11:59:26 -0300

    + creates movie details' config

--- movie-details/src/androidTest/java/com/husqvarna/movie_details/ExampleInstrumentedTest.kt
+++ movie-details/src/androidTest/java/com/husqvarna/movie_details/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.husqvarna.core
+package com.husqvarna.movie_details
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.husqvarna.core.test", appContext.packageName)
+        assertEquals("com.husqvarna.movie_details.test", appContext.packageName)
     }
 }
