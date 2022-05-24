5eccfa6f7bfbfa5fa643d99836c0c9d640b9744f
Bruno Vieira <brunovieiracrz@gmail.com>
2022-05-23 23:27:51 -0300

    - remove unused files

--- app/src/main/java/com/husqvarna/challenge/network/Headers.kt
+++
@@ -1,5 +0,0 @@
-package com.husqvarna.challenge.network
-
-enum class Headers(val value:String) {
-    API_KEY("api_key")
-}
--- app/src/main/java/com/husqvarna/challenge/network/MoviesDataSource.kt
+++
@@ -1,4 +0,0 @@
-package com.husqvarna.challenge.network
-
-interface MoviesDataSource {
-}
