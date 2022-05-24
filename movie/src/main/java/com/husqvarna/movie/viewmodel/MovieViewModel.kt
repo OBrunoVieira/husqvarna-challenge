c69ddb976db9509779900a82e8a08d6fdfeaba25
Bruno Vieira <brunovieiracrz@gmail.com>
2022-05-24 17:30:48 -0300

    + creates transformation method

--- movie/src/main/java/com/husqvarna/movie/viewmodel/MovieViewModel.kt
+++ movie/src/main/java/com/husqvarna/movie/viewmodel/MovieViewModel.kt
@@ -5,9 +5,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.husqvarna.core.viewmodel.DataSource
 import com.husqvarna.movie.domain.PopularMovieUseCase
+import com.husqvarna.movie.repository.models.Movie
 import com.husqvarna.movie.ui.models.MovieUI
 
-class MovieViewModel(private val popularMovieUseCase: PopularMovieUseCase) : ViewModel() {
+class MovieViewModel(internal val popularMovieUseCase: PopularMovieUseCase) : ViewModel() {
 
     val liveDataPopularMovies = MutableLiveData<DataSource<List<MovieUI>>>()
 
@@ -16,16 +17,7 @@ class MovieViewModel(private val popularMovieUseCase: PopularMovieUseCase) : Vie
 
         popularMovieUseCase.getTop10(viewModelScope,
             onSuccess = { movies ->
-                val data = movies?.mapIndexed { index, item ->
-                    MovieUI(
-                        item.id,
-                        item.title,
-                        index,
-                        item.voteAverage,
-                        item.posterPath
-                    )
-                }
-
+                val data = transformToMovieUI(movies)
                 liveDataPopularMovies.postValue(DataSource.success(data))
             },
             onError = {
@@ -33,4 +25,14 @@ class MovieViewModel(private val popularMovieUseCase: PopularMovieUseCase) : Vie
             })
     }
 
+    internal fun transformToMovieUI(list: List<Movie>?) =
+        list?.mapIndexed { index, item ->
+            MovieUI(
+                item.id,
+                item.title,
+                index,
+                item.voteAverage,
+                item.posterPath
+            )
+        }
 }
