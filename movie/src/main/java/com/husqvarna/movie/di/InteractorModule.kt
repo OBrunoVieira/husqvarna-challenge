e229d72619dac5274209de1375fc3b97b948acbc
Bruno Vieira <brunovieiracrz@gmail.com>
2022-05-24 18:54:59 -0300

    + updates interactor module

--- movie/src/main/java/com/husqvarna/movie/di/InteractorModule.kt
+++ movie/src/main/java/com/husqvarna/movie/di/InteractorModule.kt
@@ -1,9 +1,10 @@
 package com.husqvarna.movie.di
 
+import com.husqvarna.core.network.DefaultDispatcherProvider
 import com.husqvarna.movie.domain.PopularMovieUseCase
 import com.husqvarna.movie.domain.PopularMoviesInteractor
 import org.koin.dsl.module
 
 val moviesInteractorModule = module {
-    factory<PopularMovieUseCase> { PopularMoviesInteractor(get()) }
+    factory<PopularMovieUseCase> { PopularMoviesInteractor(DefaultDispatcherProvider(), get()) }
 }
