1d5bf5ca310101ab6ab2d47ee754601e9b0b7e92
Bruno Vieira <brunovieiracrz@gmail.com>
2022-05-24 11:52:49 -0300

    + creates error handling for home activity

--- home/src/main/java/com/husqvarna/home/ui/activities/HomeActivity.kt
+++ home/src/main/java/com/husqvarna/home/ui/activities/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.husqvarna.home.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
@@ -11,11 +12,14 @@ import com.husqvarna.core.viewmodel.DataState
 import com.husqvarna.home.databinding.ActivityHomeBinding
 import com.husqvarna.home.ui.views.MarginScalePageTransformer
 import com.husqvarna.movie.ui.adapters.MovieAdapter
+import com.husqvarna.movie.ui.listeners.PageClickListener
 import com.husqvarna.movie.ui.models.MovieUI
 import com.husqvarna.movie.viewmodel.MovieViewModel
+import com.husqvarna.movie_details.ui.activities.MovieDetailActivity
+import com.husqvarna.movie_details.ui.activities.MovieDetailActivity.Companion.EXTRA_MOVIE_ID
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), PageClickListener {
     private var activityHomeBinding: ActivityHomeBinding? = null
     private val binding by lazy { activityHomeBinding!! }
 
@@ -40,6 +44,15 @@ class HomeActivity : AppCompatActivity() {
         movieViewModel.getTop10PopularMovies()
     }
 
+    override fun onPageClicked(movie: MovieUI?) {
+        movie?.let {
+            startActivity(
+                Intent(this, MovieDetailActivity::class.java)
+                    .putExtra(EXTRA_MOVIE_ID, movie.id)
+            )
+        }
+    }
+
     private fun setupViewPager() = binding.run {
         homeViewPager.adapter = movieAdapter
         homeViewPager.offscreenPageLimit = 2
@@ -60,12 +73,14 @@ class HomeActivity : AppCompatActivity() {
     private fun observePopularMovies() = Observer<DataSource<List<MovieUI>>> {
         when (it.dataState) {
             DataState.LOADING -> {
-                binding.homeProgressBar.isVisible = true
                 binding.homeGroup.isGone = true
+                binding.homeErrorView.isGone = true
+                binding.homeProgressBar.isVisible = true
             }
 
             DataState.SUCCESS -> {
                 binding.homeProgressBar.isGone = true
+                binding.homeErrorView.isGone = true
                 binding.homeGroup.isVisible = true
 
                 it.data?.let { data ->
@@ -74,7 +89,14 @@ class HomeActivity : AppCompatActivity() {
             }
 
             DataState.ERROR -> {
-
+                binding.homeProgressBar.isGone = true
+                binding.homeGroup.isGone = true
+                binding.homeErrorView
+                    .throwable(it.throwable)
+                    .reload {
+                        movieViewModel.getTop10PopularMovies()
+                    }
+                    .show()
             }
         }
     }
