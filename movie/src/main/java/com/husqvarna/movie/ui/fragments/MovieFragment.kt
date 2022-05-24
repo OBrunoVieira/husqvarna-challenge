3e7a2666b474d52c5f600daf711648c2741d597b
Bruno Vieira <brunovieiracrz@gmail.com>
2022-05-23 23:40:56 -0300

    + updates movieFragment

--- movie/src/main/java/com/husqvarna/movie/ui/fragments/MovieFragment.kt
+++ movie/src/main/java/com/husqvarna/movie/ui/fragments/MovieFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.husqvarna.movie.BuildConfig
 import com.husqvarna.movie.databinding.FragmentMovieBinding
-import com.husqvarna.movie.ui.models.Movie
+import com.husqvarna.movie.ui.models.MovieUI
 
 class MovieFragment : Fragment() {
 
@@ -15,14 +17,14 @@ class MovieFragment : Fragment() {
     companion object {
         private const val EXTRA_MOVIE = "EXTRA_MOVIE"
 
-        fun newInstance(movie: Movie) = MovieFragment().apply {
+        fun newInstance(movie: MovieUI?) = MovieFragment().apply {
             arguments = Bundle().apply {
                 putParcelable(EXTRA_MOVIE, movie)
             }
         }
     }
 
-    private var movie: Movie? = null
+    private var movie: MovieUI? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,7 +40,10 @@ class MovieFragment : Fragment() {
             movie = it.getParcelable(EXTRA_MOVIE)
         }
 
-        binding.vhMovieTitleTextView.text = movie?.title
-        binding.vhMovieRankingTextView.text = movie?.position?.toString()
+        movie?.imagePath?.let {
+            Glide.with(this)
+                .load(BuildConfig.TMDB_IMAGE_PATH.format(it))
+                .into(binding.vhMoviePosterImageView)
+        }
     }
 }
